bbb7d6cb9813e22abcfc733d2d6ff690a4b258f4
Navabharth-Technologies <contact@navabharth.com>
2026-07-13 10:59:14 +0530

    style: change 'DATE RANGE' label to 'Date Range' for formatting consistency

--- src/components/FocusLogs.js
+++ src/components/FocusLogs.js
@@ -309,7 +309,7 @@ export default function FocusLogs({ onBack }) {
 
         {/* Filter Bar */}
         <div style={s.filterBar}>
-          <div style={s.label}><Calendar size={18} /> DATE RANGE</div>
+          <div style={s.label}><Calendar size={18} /> Date Range</div>
 
           <div style={{ ...s.dateInputBox, border: '1.5px solid #cbd5e1' }}>
             <Calendar 
